d86c7f58d7e847eece7b025dc54317758dd2c447
Kirill Levin <levinkirill@yandex.ru>
2024-01-01 03:28:17 +0300

    Finalize data

--- data.js
+++ data.js
@@ -1,23 +1,58 @@
 const wordsList = [
     {
-        1: { content: "Apple", hasCross: false },
-        2: { content: "Banana", hasCross: false },
-        3: { content: "Cherry", hasCross: false },
-        4: { content: "Date", hasCross: true, crossPosition: "top-right" },
-        5: { content: "Fig", hasCross: false },
-        6: { content: "Grapes", hasCross: true, crossPosition: "bottom-left" },
-        7: { content: "Honeydew", hasCross: false }
+        1: { content: "нам", hasCross: false },
+        2: { content: "Х", hasCross: true },
+        3: { content: "Х", hasCross: true },
+        4: { content: "некуда", hasCross: false },
+        5: { content: "пойти", hasCross: false },
+        6: { content: "пусть", hasCross: false },
+        7: { content: "идёт", hasCross: false }
     },
     {
-        1: { content: "Orange", hasCross: true, crossPosition: "bottom-right" },
-        2: { content: "Pear", hasCross: false },
-        3: { content: "Quince", hasCross: false },
-        4: { content: "Raspberry", hasCross: false },
-        5: { content: "Strawberry", hasCross: false },
-        6: { content: "Tomato", hasCross: false },
-        7: { content: "Uva", hasCross: true, crossPosition: "top-left" }
+        1: { content: "X", hasCross: true },
+        2: { content: "моё", hasCross: false },
+        3: { content: "желание", hasCross: false },
+        4: { content: "Х", hasCross: true },
+        5: { content: "всё", hasCross: false },
+        6: { content: "что", hasCross: false },
+        7: { content: "я", hasCross: false }
     },
-    // Add more sets of words as needed
+    {
+        1: { content: "на", hasCross: false },
+        2: { content: "части", hasCross: false },
+        3: { content: "ничего", hasCross: false },
+        4: { content: "Х", hasCross: true },
+        5: { content: "не", hasCross: false },
+        6: { content: "Х", hasCross: true },
+        7: { content: "поделаешь", hasCross: false }
+    },
+    {
+        1: { content: "made", hasCross: false },
+        2: { content: "X", hasCross: true },
+        3: { content: "of", hasCross: false },
+        4: { content: "ice", hasCross: false },
+        5: { content: "X", hasCross: true },
+        6: { content: "but", hasCross: false },
+        7: { content: "soon", hasCross: false }
+    },
+    {
+        1: { content: "X", hasCross: true },
+        2: { content: "can", hasCross: false },
+        3: { content: "live", hasCross: false },
+        4: { content: "without", hasCross: false },
+        5: { content: "one", hasCross: false },
+        6: { content: "another", hasCross: false },
+        7: { content: "X", hasCross: true }
+    },
+    {
+        1: { content: "broken", hasCross: false },
+        2: { content: "and", hasCross: false },
+        3: { content: "Х", hasCross: true },
+        4: { content: "it's", hasCross: false },
+        5: { content: "X", hasCross: true },
+        6: { content: "all", hasCross: false },
+        7: { content: "because", hasCross: false }
+    },
 ];
 
 export default wordsList;
