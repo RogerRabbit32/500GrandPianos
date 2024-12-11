7f6d495f49210b739cc6c160d16883588b2bd6b6
Кирилл Левин <levin_ka@devtrix.ru>
2024-12-11 23:45:14 +0100

    Updated data.js for 2024 Lumenaza Christmas party

--- data.js
+++ data.js
@@ -1,57 +1,48 @@
 const wordsList = [
     {
-        1: { content: "нам", hasCross: false },
+        1: { content: "X", hasCross: true },
+        2: { content: "my", hasCross: false },
+        3: { content: "heart", hasCross: false },
+        4: { content: "Х", hasCross: true },
+        5: { content: "but", hasCross: false },
+        6: { content: "the", hasCross: false },
+        7: { content: "very", hasCross: false }
+    },
+    {
+        1: { content: "since", hasCross: false },
         2: { content: "Х", hasCross: true },
         3: { content: "Х", hasCross: true },
-        4: { content: "некуда", hasCross: false },
-        5: { content: "пойти", hasCross: false },
-        6: { content: "пусть", hasCross: false },
-        7: { content: "идёт", hasCross: false }
+        4: { content: "we", hasCross: false },
+        5: { content: "have", hasCross: false },
+        6: { content: "no", hasCross: false },
+        7: { content: "place", hasCross: false }
     },
     {
         1: { content: "X", hasCross: true },
-        2: { content: "моё", hasCross: false },
-        3: { content: "желание", hasCross: false },
+        2: { content: "just", hasCross: false },
+        3: { content: "one", hasCross: false },
         4: { content: "Х", hasCross: true },
-        5: { content: "всё", hasCross: false },
-        6: { content: "что", hasCross: false },
-        7: { content: "я", hasCross: false }
+        5: { content: "thing", hasCross: false },
+        6: { content: "I", hasCross: false },
+        7: { content: "need", hasCross: false }
     },
     {
-        1: { content: "на", hasCross: false },
-        2: { content: "части", hasCross: false },
-        3: { content: "ничего", hasCross: false },
+        1: { content: "and", hasCross: false },
+        2: { content: "die", hasCross: false },
+        3: { content: "you", hasCross: false },
         4: { content: "Х", hasCross: true },
-        5: { content: "не", hasCross: false },
+        5: { content: "and", hasCross: false },
         6: { content: "Х", hasCross: true },
-        7: { content: "поделаешь", hasCross: false }
+        7: { content: "I", hasCross: false }
     },
     {
-        1: { content: "made", hasCross: false },
-        2: { content: "X", hasCross: true },
-        3: { content: "of", hasCross: false },
-        4: { content: "ice", hasCross: false },
-        5: { content: "X", hasCross: true },
-        6: { content: "but", hasCross: false },
-        7: { content: "soon", hasCross: false }
-    },
-    {
-        1: { content: "X", hasCross: true },
-        2: { content: "can", hasCross: false },
-        3: { content: "live", hasCross: false },
-        4: { content: "without", hasCross: false },
-        5: { content: "one", hasCross: false },
-        6: { content: "another", hasCross: false },
+        1: { content: "to", hasCross: false },
+        2: { content: "rock", hasCross: false },
+        3: { content: "X", hasCross: true },
+        4: { content: "the", hasCross: false },
+        5: { content: "night", hasCross: false },
+        6: { content: "away", hasCross: false },
         7: { content: "X", hasCross: true }
-    },
-    {
-        1: { content: "broken", hasCross: false },
-        2: { content: "and", hasCross: false },
-        3: { content: "Х", hasCross: true },
-        4: { content: "it's", hasCross: false },
-        5: { content: "X", hasCross: true },
-        6: { content: "all", hasCross: false },
-        7: { content: "because", hasCross: false }
     },
 ];
 
